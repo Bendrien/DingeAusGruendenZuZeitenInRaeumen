b05f7ec23119ea3fa1882055424974b6ceb6d845
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2017-01-18 20:44:37 +0100

    Added error handeling to the ID Parser.

--- python/SpotifyIDParser.py
+++ python/SpotifyIDParser.py
@@ -5,9 +5,10 @@ def main():
     spotify = spotipy.Spotify()
 
     with open('cleanedRawdataWithSpotifyID.json', 'w') as dataOutFile:
-        with open('cleanedRohdatenShort.json') as dataInFile:
+        with open('cleanedRohdaten.json') as dataInFile:
             cleanedRawData = json.load(dataInFile)
             result = []
+            fails = []
 
             for entry in cleanedRawData:
                 #print("Entry: " + json.dumps(entry, sort_keys=True))
@@ -26,13 +27,25 @@ def main():
                 else:
                     print("Something went wrong while getting ID: " + str(entry["ID"])
                           + " with query: " + str(entry["Query"]))
-                    print(json.dumps(response, indent=4, sort_keys=True))
+                    print(json.dumps(response, sort_keys=True))
+                    error = {}
+                    error["ID"] = entry["ID"]
+                    error["Query"] = entry["Query"]
+
+                    fails.append(error)
+
 
 
                 # Print entry
                 #print(json.dumps(entry, indent=4, sort_keys=True))
 
         json.dump(result, dataOutFile, indent=4, sort_keys=True)
+
+        with open('IdErrors.json', 'w') as errorFile:
+            json.dump(fails, errorFile, indent=4, sort_keys=True)
+
+        print("Errors: " + str(len(fails)) + " of " + str(len(cleanedRawData)) + " possible fuckups!")
+
 
 if(__name__ == '__main__'):
     main()
